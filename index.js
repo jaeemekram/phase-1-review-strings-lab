0c59edf6dd1c98beb2c8e46cea5f144baad6c59c
Jaeem Ekram <jekram98@gmail.com>
2021-04-29 14:30:44 -0400

    commit tree 046a780f8e44589dc34104b62f47a6f766f2a2f0

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Jaeem Ekram'
+let welcomeMessage = 'Welcome to Flatbook, Jaeem Ekram!'
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, JAEEM EKRAM!'
+let shortGreeting = 'Welcome, J!'
